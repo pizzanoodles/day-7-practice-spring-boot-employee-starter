888bc1bc2c1e91c2405cc3805a3f372d7db7851c
Jens Jovellano <jensjovellano@gmail.com>
2023-08-22 19:15:44 +0800

    Feat: Added function for Get Request Mapping to list all companies.

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,12 +1,19 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("companies")
+import java.util.List;
+
+@RequestMapping("/companies")
 @RestController
 public class CompanyController {
     @Autowired
     CompanyRepository companyRepository;
+    @GetMapping
+    public List<Company> listAllCompanies() {
+        return companyRepository.listAllCompanies();
+    }
 }
