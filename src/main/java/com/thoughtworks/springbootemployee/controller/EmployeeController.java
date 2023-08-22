676f3efdce09d33336c39c40f3ff908037c268c3
Jens Jovellano <jensjovellano@gmail.com>
2023-08-22 18:55:25 +0800

    Feat: Added get request mapping to list employees depending on pageNumber and pageSize.

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -11,32 +11,42 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
+
     @GetMapping()
     public List<Employee> listAll() {
         return employeeRepository.listAll();
     }
+
     @GetMapping("/{id}")
     public Employee findById(@PathVariable Long id) {
         return employeeRepository.findById(id);
     }
+
     @GetMapping(params = {"gender"})
     public List<Employee> findByGender(@RequestParam String gender) {
         return employeeRepository.findByGender(gender);
     }
 
     @PostMapping()
-    public Employee createEmployee(@RequestBody Employee employee){
+    public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.createEmployee(employee);
     }
+
     @PutMapping("/updateEmployee/{id}")
     public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee) {
         return employeeRepository.updateEmployee(id, updatedEmployee);
     }
+
     @DeleteMapping("/deleteEmployee/{id}")
     public String deleteEmployee(@PathVariable Long id) {
-        if (!employeeRepository.deleteEmployee(id)){
+        if (!employeeRepository.deleteEmployee(id)) {
             throw new EmployeeNotFoundException();
         }
         return "Successfully deleted.";
+    }
+
+    @GetMapping(params = {"pageNumber", "pageSize"})
+    public List<Employee> listByPage(@RequestParam Long pageNumber, @RequestParam Long pageSize) {
+        return employeeRepository.listByPage(pageNumber, pageSize);
     }
 }
