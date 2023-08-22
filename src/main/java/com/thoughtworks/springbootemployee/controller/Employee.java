718738d51cbfbc03fd8ff4e34daaf683c7632deb
Jens Jovellano <jensjovellano@gmail.com>
2023-08-22 19:59:56 +0800

    Refactor: Added companyId property to Employee.

--- src/main/java/com/thoughtworks/springbootemployee/controller/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/Employee.java
@@ -2,13 +2,15 @@ package com.thoughtworks.springbootemployee.controller;
 
 public class Employee {
     private Long id;
+    private Long companyId;
     private String name;
     private Integer age;
     private String gender;
     private Integer salary;
 
-    public Employee(long id, String name, Integer age, String gender, Integer salary) {
+    public Employee(long id, Long companyId, String name, Integer age, String gender, Integer salary) {
         this.id = id;
+        this.companyId = companyId;
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -32,12 +34,20 @@ public class Employee {
         this.salary = salary;
     }
 
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
     public Long getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
     }
 
     public Integer getAge() {
