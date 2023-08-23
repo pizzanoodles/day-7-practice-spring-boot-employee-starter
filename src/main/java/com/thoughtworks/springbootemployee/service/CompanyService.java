9775ec33198b257794659f09690ff24264ad7e6e
Jens Jovellano <jensjovellano@gmail.com>
2023-08-23 22:17:17 +0800

    Feat: created Company Service class and added functions for getting list of companies, company by id, and employees by company id called in Company Repository.

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Company> listAllCompanies() {
+        return companyRepository.listAllCompanies();
+    }
+
+    public Company getCompanyById(Long id) {
+        return companyRepository.getCompanyById(id);
+    }
+
+    public List<Employee> getEmployeesByCompanyId(Long id) {
+        return companyRepository.getEmployeesByCompanyId(id);
+    }
+}
