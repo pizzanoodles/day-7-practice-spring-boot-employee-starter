a2365f464a7a0e206ddbea6c64af2a4cd6f1a48d
Jens Jovellano <jensjovellano@gmail.com>
2023-08-23 18:02:15 +0800

    Feat: Created EmployeeCreateException

---
+++ src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeCreateException.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.springbootemployee.exception;
+
+public class EmployeeCreateException extends RuntimeException{
+    public EmployeeCreateException() {
+        super("Employee must be 18~65 years old");
+    }
+}
