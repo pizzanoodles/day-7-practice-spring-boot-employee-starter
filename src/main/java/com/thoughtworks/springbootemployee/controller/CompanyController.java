a8f57098c98a5cbc3f2b25e864e90177fb8596c0
Jens Jovellano <jensjovellano@gmail.com>
2023-08-23 22:44:04 +0800

    Refactor: converted repository calls intro service calls.

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -4,55 +4,54 @@ import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.exception.CompanyNotFoundException;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @SuppressWarnings("all")
 @RequestMapping("/companies")
 @RestController
 public class CompanyController {
     @Autowired
-    CompanyRepository companyRepository;
+    CompanyService companyService;
 
     @GetMapping
     public List<Company> listAllCompanies() {
-        return companyRepository.listAllCompanies();
+        return companyService.listAllCompanies();
     }
 
     @GetMapping("/{id}")
     public Company getCompanyById(@PathVariable Long id) {
-        return companyRepository.getCompanyById(id);
+        return companyService.getCompanyById(id);
     }
 
     @GetMapping("/{id}/employees")
     public List<Employee> listEmployeesByCompany(@PathVariable Long id) {
-        return companyRepository.getEmployeesByCompanyId(id);
+        return companyService.getEmployeesByCompanyId(id);
     }
 
     @GetMapping(params = {"pageNumber", "pageSize"})
     public List<Company> listCompaniesByPage(@RequestParam Long pageNumber, @RequestParam Long pageSize) {
-        return companyRepository.listCompaniesByPage(pageNumber, pageSize);
+        return companyService.getCompaniesByPage(pageNumber, pageSize);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Company addCompany(@RequestBody Company company) {
-        return companyRepository.addCompany(company);
+        return companyService.createCompany(company);
     }
 
     @PutMapping("/{id}")
     public Company updateCompany(@PathVariable Long id, @RequestBody Company updatedCompany) {
-        return companyRepository.updateCompany(id, updatedCompany);
+        return companyService.updateCompany(id, updatedCompany);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String deleteCompany(@PathVariable Long id) {
-        if (!companyRepository.deleteCompany(id)) {
-            throw new CompanyNotFoundException();
-        }
-        return "Successfully deleted.";
+    public void deleteCompany(@PathVariable Long id) {
+        companyService.deleteCompany(id);
     }
 }
