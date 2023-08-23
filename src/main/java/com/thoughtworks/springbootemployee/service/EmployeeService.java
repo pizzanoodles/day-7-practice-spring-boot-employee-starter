940e6c5457aab6d4765dc7e3913ad03c6e6cfa41
Jens Jovellano <jensjovellano@gmail.com>
2023-08-23 21:36:35 +0800

    Fix: Added @Service annotation to the class.

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -3,9 +3,10 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.exception.EmployeeCreateException;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
